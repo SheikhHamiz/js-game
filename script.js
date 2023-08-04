b54c4f3e12fa925532d6c21bab57c5cfccf15bf3
SheikhHamiz <72609103+SheikhHamiz@users.noreply.github.com>
2023-08-04 17:52:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,36 @@ function computerPlay() {
       return 'Scissors';
     }
   }
-    console.log(computerPlay());
+    // console.log(computerPlay());
+
+function playRound(userInput) {
+  computerInput = computerPlay();
+  console.log(computerInput);
+  switch(computerInput.toLowerCase()) {
+    case 'rock':
+      if(userInput === 'paper'){
+        return 'you win';
+      } else if(userInput === 'scissors') {
+        return 'you lose';
+      } else {
+        return 'draw'
+      }
+    case 'paper':
+      if(userInput === 'rock') {
+        return 'you lose';
+      } else if(userInput === 'scissors') {
+        return 'you win';
+      } else {
+        return 'draw';
+      }
+    case 'scissors':
+      if(userInput === 'rock') {
+        return 'you win';
+      } else if(userInput === 'paper') {
+        return 'you lose';
+      } else {
+        return 'draw';
+      }
+  }
+}
+console.log(playRound("paper"));
