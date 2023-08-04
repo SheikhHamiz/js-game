cdc785ca51a957fde95f885d9f9516d161363186
SheikhHamiz <72609103+SheikhHamiz@users.noreply.github.com>
2023-08-04 22:23:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,29 +12,29 @@ function computerPlay() {
 
 function playRound(userInput) {
   computerInput = computerPlay();
-  console.log(computerInput);
+  console.log(`computer chose ${computerInput}`);
   switch(computerInput.toLowerCase()) {
     case 'rock':
       if(userInput === 'paper'){
-        return 'you win';
+        return 'You Win!';
       } else if(userInput === 'scissors') {
-        return 'you lose';
+        return 'You Lose!';
       } else {
         return 'draw'
       }
     case 'paper':
       if(userInput === 'rock') {
-        return 'you lose';
+        return 'You Lose!';
       } else if(userInput === 'scissors') {
-        return 'you win';
+        return 'You Win!';
       } else {
         return 'draw';
       }
     case 'scissors':
       if(userInput === 'rock') {
-        return 'you win';
+        return 'You Win!';
       } else if(userInput === 'paper') {
-        return 'you lose';
+        return 'You Lose!';
       } else {
         return 'draw';
       }
@@ -44,22 +44,30 @@ function playRound(userInput) {
 function game() {
     let playerScore = 0;
     let computerScore = 0;
+    let draw = 0;
 
     for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Round " + (i + 1) + ": Enter 'Rock', 'Paper', or 'Scissors'").trim();
+        let playerSelection = prompt("Round " + (i + 1) + ": Enter 'Rock', 'Paper', or 'Scissors'").trim();
+        while (playerSelection.toLowerCase() !== 'rock' && playerSelection.toLowerCase() !== 'paper' &&
+              playerSelection.toLowerCase() !== 'scissors') 
+        {
+                playerSelection = prompt("Round " + (i + 1) + ": Enter 'Rock', 'Paper', or 'Scissors'").trim();
+        }
+        console.log(`You chose ${playerSelection}`);
         const computerSelection = computerPlay();
-        const roundResult = playRound(playerSelection, computerSelection);
-
+        const roundResult = playRound(playerSelection.toLowerCase(), computerSelection);
         console.log(roundResult);
 
         if (roundResult.startsWith("You Win!")) {
             playerScore++;
         } else if (roundResult.startsWith("You Lose!")) {
             computerScore++;
+        } else {
+          draw++;
         }
     }
 
     console.log("Game Over!");
-    console.log(`Final Scores: You: ${playerScore} | Computer: ${computerScore}`);
+    console.log(`Final Scores: You: ${playerScore} | Computer: ${computerScore} | Draw: ${draw}`);
 }
 game();
