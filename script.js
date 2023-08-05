e2f2bb45644b69a30164d04cabe1ca1bf94a50e3
SheikhHamiz <72609103+SheikhHamiz@users.noreply.github.com>
2023-08-05 17:58:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function computerPlay() {
-    const randomNumber = Math.random();
-    if (randomNumber < 0.33) {
-      return 'Rock';
-    } else if (randomNumber < 0.66) {
-      return 'Paper';
-    } else {
-      return 'Scissors';
-    }
+  const randomNumber = Math.random();
+  if (randomNumber < 0.33) {
+    return 'Rock';
+  } else if (randomNumber < 0.66) {
+    return 'Paper';
+  } else {
+    return 'Scissors';
   }
-    // console.log(computerPlay());
+}
 
 function playRound(userInput) {
   computerInput = computerPlay();
@@ -39,35 +38,41 @@ function playRound(userInput) {
         return 'draw';
       }
   }
-}
-// console.log(playRound("paper"));
+  }
 function game() {
+  let playAgain = true;
+
+  while (playAgain) {
     let playerScore = 0;
     let computerScore = 0;
     let draw = 0;
 
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Round " + (i + 1) + ": Enter 'Rock', 'Paper', or 'Scissors'").trim();
-        while (playerSelection.toLowerCase() !== 'rock' && playerSelection.toLowerCase() !== 'paper' &&
-              playerSelection.toLowerCase() !== 'scissors') 
-        {
-                playerSelection = prompt("Round " + (i + 1) + ": Enter 'Rock', 'Paper', or 'Scissors'").trim();
-        }
-        console.log(`You chose ${playerSelection}`);
-        const computerSelection = computerPlay();
-        const roundResult = playRound(playerSelection.toLowerCase(), computerSelection);
-        console.log(roundResult);
+      let playerSelection = prompt("Round " + (i + 1) + ": Enter 'Rock', 'Paper', or 'Scissors'").trim();
+      while (playerSelection.toLowerCase() !== 'rock' && playerSelection.toLowerCase() !== 'paper' &&
+            playerSelection.toLowerCase() !== 'scissors') 
+      {
+              playerSelection = prompt("Round " + (i + 1) + ": Enter 'Rock', 'Paper', or 'Scissors'").trim();
+      }
+      console.log(`You chose ${playerSelection}`);
+      const computerSelection = computerPlay();
+      const roundResult = playRound(playerSelection.toLowerCase(), computerSelection);
+      console.log(roundResult);
 
-        if (roundResult.startsWith("You Win!")) {
-            playerScore++;
-        } else if (roundResult.startsWith("You Lose!")) {
-            computerScore++;
-        } else {
-          draw++;
-        }
-    }
+      if (roundResult.startsWith("You Win!")) {
+          playerScore++;
+      } else if (roundResult.startsWith("You Lose!")) {
+          computerScore++;
+      } else {
+        draw++;
+      }
+  }
 
     console.log("Game Over!");
     console.log(`Final Scores: You: ${playerScore} | Computer: ${computerScore} | Draw: ${draw}`);
+
+    // Ask the user if they want to play again
+    const playAgainInput = prompt("Do you want to play again? (yes/no)").trim().toLowerCase();
+    playAgain = playAgainInput === "yes";
+  }
 }
-game();
